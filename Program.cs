4e518391c3afb3e364b595e13615fc99dfc60c62
Iuliia_Mazhara <tm41mazhara@gmail.com>
2022-11-14 18:52:00 +0100

    The homework task 1

--- Program.cs
+++ Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*Create console application which as argument accept string value (string1) and accept additional string from console (string 2).
+    String1 and string2 should be join in one single string. Joined string should be outputted in console. 
+    After that user should enter one more string (string3). String 3 should be removed from joined string, result string after remove should be outputted in console  */
+
 namespace ReskillProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+           
         }
     }
 }
